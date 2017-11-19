17cc8626af57b88862ccd4109b070c216dc39de6
Phakinchu <phakinchu@gmail.com>
2017-11-20 05:33:45 +0700

    top bar make

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,3 +1,4 @@
+
 <nav class="navbar navbar-inverse">
 <div class="container-fluid">
   <!-- Brand and toggle get grouped for better mobile display -->
@@ -8,20 +9,21 @@
       <span class="icon-bar"></span>
       <span class="icon-bar"></span>
     </button>
-    <a class="navbar-brand" href="#">Prison Break</a>
+    
+    <a class="navbar-brand" href="/">Home</a>
   </div>
 
   <!-- Collect the nav links, forms, and other content for toggling -->
   <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
     <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Prisoner List<span class="sr-only">(current)</span></a></li>
-      <li><a href="#">Officer List</a></li>
+      <li class="active"><a href="/prisoners">Prisoner List<span class="sr-only">(current)</span></a></li>
+      <li><a href="/officers">Officer List</a></li>
       <li class="dropdown">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Detail Pri <span class="caret"></span></a>
         <ul class="dropdown-menu">
-          <li><a href="#">Action</a></li>
-          <li><a href="#">Another action</a></li>
-          <li><a href="#">Something else here</a></li>
+          <li><a href="/prisonerbehavioralhistories">behavioralhistories</a></li>
+          <li><a href="/prisonereducepunishmenthistories">reducepunishmenthistories</a></li>
+          <li><a href="/freeprisonhistories">freeprisonhistories</a></li>
           <li role="separator" class="divider"></li>
           <li><a href="#">Separated link</a></li>
           <li role="separator" class="divider"></li>
--- resources/views/posts/prisonerindex.blade.php
+++ resources/views/posts/prisonerindex.blade.php
@@ -3,38 +3,34 @@
 @section('content')
 <div class="blog-header">
   <h1 class="blog-title">Prisoner List</h1>
-  <p class="lead blog-description">All Prisoner</p>
-  <div class="panel panel-default">
-    <div class="panel-heading">Prisoner List</div>
-    <!-- <table class="table">
-      <tr> 
-  	      <td>prisoner</td>
-          <td>prisoner_fname</td>
-  	      <td>prisoner_lname</td>
-  	      <td>gender</td>
-          <td>dob</td>
-  	      <td>add</td>
-          <td>id_officer</td>
-          <td>id_level</td>
-          <td>score</td>
-  	      <td>Edit</td>
-  	      <td>Delete</td>
-      </tr> -->
-    @foreach ($posts as $post)    
-      <table class="table">
-        <tr>
-			    <td>{{$post->id_prisoner}}</td>
-			    <td>{{$post->fname}}</td>
-			    <td>{{$post->lname}}</td>
-			    <td>{{$post->gender}}</td>
-          <td>{{$post->dob}}</td>
-			    <td>{{$post->address}}</td>
-          <td>{{$post->id_officer}}</td>
-          <td>{{$post->id_level}}</td>
-          <td>{{$post->scorepri}}</td>
-			    <td><a href="/prisoner/{{$post->id_prisoner}}/editpage">Click</a></center></td>
-			    <td><a href="/prisoner/{{$post->id_prisoner}}/delete">Click</a></center></td>
-			  </tr>
+	<p class="lead blog-description">All prisoners</p>
+  @foreach ($posts as $post)
+  <table border=2><tr> 
+  	<td><center>Prisoner ID</center></td>
+  	<td><center>fname</center></td>
+  	<td><center>lname</center></td>
+  	<td><center>address</center></td>
+    <td><center>dob</center></td>
+  	<td><center>gender</center></td>
+    <td><center>scorepri</center></td>
+    <td><center>id_officer</center></td>
+    <td><center>id_level</center></td>
+  	<td><center>Edit</center></td>
+  	<td><center>Delete</center></td>
+    </tr>
+    <tr>
+		<td><center>{{$post->id_prisoner}}</center></td>
+		<td><center>{{$post->fname}}</center></td>
+		<td><center>{{$post->lname}}</center></td>
+		<td><center>{{$post->address}}</center></td>
+        <td><center>{{$post->dob}}</center></td>
+		<td><center>{{$post->gender}}</center></td>
+        <td><center>{{$post->scorepri}}</center></td>
+        <td><center>{{$post->id_officer}}</center></td>
+        <td><center>{{$post->id_level}}</center></td>
+		<td><center><a href="/prisoner/{{$post->id_prisoner}}/editpage">Click</a></center></td>
+		<td><center><a href="/prisoner/{{$post->id_prisoner}}/delete">Click</a></center></td>
+	</tr>
     </table><br> 
 
   @endforeach
